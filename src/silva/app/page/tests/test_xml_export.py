018de85a2ca42f9b79d58da78432be4f98e87b91
Sylvain Viollon <thefunny@gmail.com>
2013-08-30 13:34:59 +0200

    Update tests.

--- src/silva/app/page/tests/test_xml_export.py
+++ src/silva/app/page/tests/test_xml_export.py
@@ -5,7 +5,7 @@
 import unittest
 from datetime import datetime
 
-from Products.Silva.testing import TestRequest
+from Products.Silva.testing import TestRequest, Transaction
 from Products.Silva.tests.test_xml_export import SilvaXMLTestCase
 
 from silva.core.contentlayout.interfaces import IBlockController, IBlockManager
@@ -25,14 +25,15 @@ class PageXMLExportTestCase(SilvaXMLTestCase):
 
     def setUp(self):
         self.root = self.layer.get_application()
-        self.layer.login('editor')
-        factory = self.root.manage_addProduct['Silva']
-        factory.manage_addFolder('base', 'Export base')
-        factory = self.root.base.manage_addProduct['silva.app.page']
-        factory.manage_addPage('page', 'Page')
-        design = registry.lookup_design_by_name('adesign')
-        version = self.root.base.page.get_editable()
-        version.set_design(design)
+        with Transaction():
+            self.layer.login('editor')
+            factory = self.root.manage_addProduct['Silva']
+            factory.manage_addFolder('base', 'Export base')
+            factory = self.root.base.manage_addProduct['silva.app.page']
+            factory.manage_addPage('page', 'Page')
+            design = registry.lookup_design_by_name('adesign')
+            version = self.root.base.page.get_editable()
+            version.set_design(design)
 
     def test_export_page(self):
         """Export a simple page.
@@ -47,30 +48,32 @@ class PageXMLExportTestCase(SilvaXMLTestCase):
     def test_export_page_with_page_model(self):
         """Export a page with a page model at the same time.
         """
-        factory = self.root.base.manage_addProduct['silva.core.contentlayout']
-        factory.manage_addPageModel('model', 'A Page Model')
+        with Transaction():
+            factory = self.root.base.manage_addProduct['silva.core.contentlayout']
+            factory.manage_addPageModel('model', 'A Page Model')
 
-        model = self.root.base.model
-        model_version = model.get_editable()
-        model_version.set_design(registry.lookup_design_by_name('adesign'))
-        IPublicationWorkflow(model).publish()
+            model = self.root.base.model
+            model_version = model.get_editable()
+            model_version.set_design(registry.lookup_design_by_name('adesign'))
+            IPublicationWorkflow(model).publish()
 
-        text_block = TextBlock(identifier='text block 1')
-        controller = getWrapper(
-            (text_block, model_version, TestRequest()),
-            IBlockController)
-        controller.text = "<div>text</div>"
+            text_block = TextBlock(identifier='text block 1')
+            controller = getWrapper(
+                (text_block, model_version, TestRequest()),
+                IBlockController)
+            controller.text = "<div>text</div>"
 
-        manager = IBlockManager(model_version)
-        manager.add('two', text_block)
-        manager.add('two', BlockSlot(identifier='slot-for-two'))
-        manager.add('one', BlockSlot(
-                identifier='slot-for-one',
-                tag='div',
-                css_class='the-only-one'))
+            manager = IBlockManager(model_version)
+            manager.add('two', text_block)
+            manager.add('two', BlockSlot(identifier='slot-for-two'))
+            manager.add('one', BlockSlot(
+                    identifier='slot-for-one',
+                    tag='div',
+                    css_class='the-only-one'))
 
-        page_version = self.root.base.page.get_editable()
-        page_version.set_design(model_version)
+            page_version = self.root.base.page.get_editable()
+            page_version.set_design(model_version)
+
         self.assertEquals(model_version, page_version.get_design())
 
         exporter = self.assertExportEqual(
@@ -84,13 +87,15 @@ class PageXMLExportTestCase(SilvaXMLTestCase):
         """Export a page that uses a page model that is not inside the
         export folder.
         """
-        factory = self.root.manage_addProduct['silva.core.contentlayout']
-        factory.manage_addPageModel('model', 'A Page Model')
-        IPublicationWorkflow(self.root.model).publish()
+        with Transaction():
+            factory = self.root.manage_addProduct['silva.core.contentlayout']
+            factory.manage_addPageModel('model', 'A Page Model')
+            IPublicationWorkflow(self.root.model).publish()
 
-        model_version = self.root.model.get_viewable()
-        page_version = self.root.base.page.get_editable()
-        page_version.set_design(model_version)
+            model_version = self.root.model.get_viewable()
+            page_version = self.root.base.page.get_editable()
+            page_version.set_design(model_version)
+
         self.assertEquals(model_version, page_version.get_design())
 
         exporter = self.assertExportEqual(
@@ -108,25 +113,28 @@ class NewsPageXMLExportTestCase(SilvaXMLTestCase):
     def setUp(self):
         # adding base folder
         self.root = self.layer.get_application()
-        self.layer.login('editor')
-
-        factory = self.root.manage_addProduct['silva.app.news']
-        factory.manage_addNewsPublication('news', 'News')
-        self.root.news.filter.set_show_agenda_items(True)
+        with Transaction():
+            self.layer.login('editor')
+            factory = self.root.manage_addProduct['silva.app.news']
+            factory.manage_addNewsPublication('news', 'News')
+            self.root.news.filter.set_show_agenda_items(True)
 
     def test_export_news_info_page(self):
         # adding news page
-        factory = self.root.news.manage_addProduct['silva.app.page']
-        factory.manage_addNewsPage('newspage', 'A News Page')
+        with Transaction():
+            factory = self.root.news.manage_addProduct['silva.app.page']
+            factory.manage_addNewsPage('newspage', 'A News Page')
+
         version = self.root.news.newspage.get_editable()
         design = registry.lookup_design_by_name('adesign')
         self.assertIsNotNone(design)
         self.assertIsNotNone(version)
 
-        version.set_design(design)
-        version.set_subjects(['all'])
-        version.set_target_audiences(['generic'])
-        version.set_display_datetime(datetime(2010, 9, 30, 10, 0, 0))
+        with Transaction():
+            version.set_design(design)
+            version.set_subjects(['all'])
+            version.set_target_audiences(['generic'])
+            version.set_display_datetime(datetime(2010, 9, 30, 10, 0, 0))
 
         # adding blocks
         IBlockManager(version).add('one', NewsInfoBlock())
@@ -140,17 +148,19 @@ class NewsPageXMLExportTestCase(SilvaXMLTestCase):
 
     def test_export_agenda_info_page(self):
         # adding agenda page
-        factory = self.root.news.manage_addProduct['silva.app.page']
-        factory.manage_addAgendaPage('agendapage', 'An Agenda Page')
-        version = self.root.news.agendapage.get_editable()
-        design = registry.lookup_design_by_name('adesign')
+        with Transaction():
+            factory = self.root.news.manage_addProduct['silva.app.page']
+            factory.manage_addAgendaPage('agendapage', 'An Agenda Page')
+            version = self.root.news.agendapage.get_editable()
+            design = registry.lookup_design_by_name('adesign')
+
         self.assertIsNotNone(version)
         self.assertIsNotNone(design)
 
-        version.set_design(design)
-
-        # adding block
-        IBlockManager(version).add('one', AgendaInfoBlock())
+        with Transaction():
+            version.set_design(design)
+            # adding block
+            IBlockManager(version).add('one', AgendaInfoBlock())
 
         exporter = self.assertExportEqual(
             self.root.news,
